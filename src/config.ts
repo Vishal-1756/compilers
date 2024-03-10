fe67efdcf1e0e5025d7417d266297bc472fef190
Vishal-Borase <126957307+Vishal-1756@users.noreply.github.com>
2024-03-10 13:27:39 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
 import * as tp from "./interfaces";
 
 // Configure Your own bot free bot source code for helping students 
-let ownerId = 1791106582
+let ownerId = 6312693124
 
 let config: tp.Config = { ttl: 60};
 
 function configure(cnf: tp.Config) {
   config = cnf;
 }
-config.version = "3.1.0"
-config.versionNo = 26
+config.version = "34.1.0"
+config.versionNo = 30
 config.ttl = 60;
 
 if(!config.startSymbol)
 config.startSymbol = "/"
 if(!config.group)
-config.group = "@Logicb_support";
+config.group = "@Not_Coding_Suport";
 if(!config.channel)
-config.channel = "@LogicBots";
+config.channel = "@Not_Coding";
 config.configure = configure;
 export default config;
 
